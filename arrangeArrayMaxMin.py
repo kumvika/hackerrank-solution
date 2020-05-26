7a8d0076f159360ef05b46e73b8f506d10fa8f80
vikash252 <vikash@exotel.in>
2020-05-27 00:15:59 +0530

    [leetcode] Added one more solution for Rearrange array Max Min

--- arrangeArrayMaxMin.py
+++ arrangeArrayMaxMin.py
@@ -1,3 +1,4 @@
+# Method 1: with T.C - o(N) and S.C O(N)
 def rearrangeMaxMin(nums):
     # Time Complexity O(N)
     # Space Complexity O(N)
@@ -15,7 +16,23 @@ def rearrangeMaxMin(nums):
         count += 1
     return output_nums
 
+# Method 2: with T.C - o(N2) and S.C O(1)
+def rearrangeMaxMin2(nums):
+    # Time Complexity O(N2)
+    # Space Complexity O(1)
+    for i in range(len(nums)-1):
+        if i % 2 == 0:
+            max_num = nums[len(nums)-1]
+            temp = nums[i]
+            for j in range(len(nums)-2, i, -1):
+                nums[j+1] = nums[j]
+            nums[i] = max_num
+            nums[i+1] = temp
+    return nums
+            
+
 nums = [1,2,4,5,9]
-print(rearrangeMaxMin(nums))
+#nums = [1,2,3,4]
+print(rearrangeMaxMin2(nums))
 
 
